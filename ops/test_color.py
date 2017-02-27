c5d2452cefe01ab99fda21470f66ec991a68f7bf
Cameron Fabbri <cameronfabbri@gmail.com>
2017-02-27 15:56:08 -0600

    update

--- ops/test_color.py
+++ ops/test_color.py
@@ -3,13 +3,38 @@ from skimage import color
 import cv2
 import numpy as np
 
-img = misc.imread('Image.jpg')
-img = color.rgb2lab(img)
-print type(img[0][0][0])
-exit()
-#gray = img[:,:,0]
-img = 255*color.lab2rgb(img)
+import time
+s = time.time()
 
+color_imgs = np.empty((64, 256, 256, 3), dtype=np.float32)
+gray_imgs = np.empty((64, 256, 256, 1), dtype=np.float32)
+
+for i in range(64):
+   # read image
+   img      = misc.imread('rgb.jpg')
+   
+   # convert to gray
+   gray_img = color.rgb2gray(img)
+
+   # resize
+   img = misc.imresize(img, (256, 256))
+   gray_img = misc.imresize(gray_img, (256, 256))
+   gray_img = np.expand_dims(gray_img, 2) 
+   # convert to LAB
+   img = color.rgb2lab(img)
+
+   # normalize
+   img = img/127.5 - 1.
+   gray_img = gray_img/127.5 - 1.
+  
+   color_imgs[i, ...] = img
+   gray_imgs[i, ...] = gray_img
+
+print time.time()-s
+exit()
+#misc.imsave('lab.jpg', img)
+img = misc.imread('lab.jpg')
+img = 255.0*color.lab2rgb(np.uint8(img))
 misc.imsave('conv.jpg', img)
 #misc.imsave('gray.JPEG', gray)
 
