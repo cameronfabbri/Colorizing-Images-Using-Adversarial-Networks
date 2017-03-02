879bd680ece617f98be268996aa9d90bcb87671c
Cameron Fabbri <cameronfabbri@gmail.com>
2017-03-01 19:58:10 -0600

    started ebgans

---
+++ ebgans/architecture.py
@@ -0,0 +1,102 @@
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import sys
+
+sys.path.insert(0, 'ops/')
+from tf_ops import lrelu
+
+def mse(pred, real):
+   return tf.reduce_mean(tf.sqrt(2*tf.nn.l2_loss(pred-real)))
+
+def netG(z, batch_size):
+
+   print 'GENERATOR'
+   z = slim.fully_connected(z, 4*4*1024, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='g_z')
+   z = tf.reshape(z, [batch_size, 4, 4, 1024])
+   print 'z:',z
+
+   print 'z:',z
+   conv1 = slim.convolution2d_transpose(z, 512, 5, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='g_conv1')
+   conv1 = tf.nn.relu(conv1)
+   print 'conv1:',conv1
+
+   conv2 = slim.convolution2d_transpose(conv1, 256, 5, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='g_conv2')
+   conv2 = tf.nn.relu(conv2)
+   print 'conv2:',conv2
+   
+   conv3 = slim.convolution2d_transpose(conv2, 128, 5, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='g_conv3')
+   conv3 = tf.nn.relu(conv3)
+   print 'conv3:',conv3
+
+   conv4 = slim.convolution2d_transpose(conv3, 3, 5, stride=2, activation_fn=tf.identity, scope='g_conv4')
+   conv4 = tf.nn.tanh(conv4)
+   print 'conv4:',conv4
+   print
+   print 'END G'
+   print
+
+   tf.add_to_collection('vars', conv1)
+   tf.add_to_collection('vars', conv2)
+   tf.add_to_collection('vars', conv3)
+   tf.add_to_collection('vars', conv4)
+
+   return conv4 
+
+
+def netD(input_images, batch_size, reuse=False):
+   encoded = encoder(input_images, batch_size, reuse=reuse)
+   decoded = decoder(encoded, batch_size, reuse=reuse)
+   return encoded, decoded, mse(decoded, input_images)
+
+
+'''
+'''
+def encoder(input_images, batch_size, reuse=False):
+   print 'DISCRIMINATOR' 
+   sc = tf.get_variable_scope()
+   with tf.variable_scope(sc, reuse=reuse):
+
+      print 'input images:',input_images
+      conv1 = slim.convolution(input_images, 64, 4, stride=2, activation_fn=tf.identity, scope='d_conv1')
+      conv1 = lrelu(conv1)
+      print 'conv1:',conv1
+
+      conv2 = slim.convolution(conv1, 128, 4, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='d_conv2')
+      conv2 = lrelu(conv2)
+      print 'conv2:',conv2
+      
+      conv3 = slim.convolution(conv2, 256, 5, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='d_conv3')
+      conv3 = lrelu(conv3)
+      print 'conv3:',conv3
+
+      conv4 = slim.convolution2d_transpose(conv3, 128, 4, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='d_conv4')
+      conv4 = lrelu(conv4)
+      print 'conv4:',conv4
+      
+      tf.add_to_collection('vars', conv1)
+      tf.add_to_collection('vars', conv2)
+      tf.add_to_collection('vars', conv3)
+      tf.add_to_collection('vars', conv4)
+
+      return conv4
+
+
+def decoder(encoded, batch_size, reuse=False):
+   sc = tf.get_variable_scope()
+   with tf.variable_scope(sc, reuse=reuse):
+
+      conv5 = slim.convolution2d_transpose(encoded, 64, 4, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='d_conv5')
+      conv5 = lrelu(conv5)
+      print 'conv5:',conv5
+
+      conv6 = slim.convolution2d_transpose(conv5, 3, 4, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='d_conv6')
+      conv6 = lrelu(conv6)
+      print 'conv6:',conv6
+      
+      print 'END D\n'
+
+      tf.add_to_collection('vars', conv5)
+      tf.add_to_collection('vars', conv6)
+
+      return conv6
+
---
+++ ebgans/train.py
@@ -0,0 +1,133 @@
+import tensorflow as tf
+from architecture import encoder, decoder, netD, netG, mse
+import numpy as np
+import random
+import ntpath
+import sys
+import cv2
+import os
+
+import loadceleba
+
+'''
+   Builds the graph and sets up params, then starts training
+'''
+def buildAndTrain(info):
+
+   checkpoint_dir = info['checkpoint_dir']
+   batch_size     = info['batch_size']
+   dataset        = info['dataset']
+   load           = info['load']
+   gray           = info['load']
+
+   # load data
+   image_data = loadceleba.load(load=load)
+
+   # placeholders for data going into the network
+   global_step = tf.Variable(0, name='global_step', trainable=False)
+   real_images = tf.placeholder(tf.float32, shape=(batch_size, 64, 64, 3), name='color_images')
+   z           = tf.placeholder(tf.float32, shape=(batch_size, 100), name='z')
+
+   # generated images
+   gen_images = netG(z, batch_size)
+
+   D_loss_real, encoded_real, decoded_real = netD(real_images, batch_size)
+   D_loss_fake, encoded_fake, decoded_fake = netD(gen_images, batch_size, reuse=True)
+
+   # margin for celeba
+   margin = 20
+
+   # cost functions
+   errD = tf.reduce_mean(D_loss_real + tf.maximum(margin-D_loss_fake, 0))
+   #errD = tf.reduce_mean(margin - D_loss_fake + D_loss_real)
+   errG = tf.reduce_mean(D_loss_fake)
+
+   # tensorboard summaries
+   tf.summary.scalar('d_loss', errD)
+   tf.summary.scalar('g_loss', errG)
+   tf.summary.image('real_images', real_images, max_outputs=batch_size)
+   tf.summary.image('generated_images', gen_images, max_outputs=batch_size)
+   merged_summary_op = tf.summary.merge_all()
+
+   # get all trainable variables, and split by network G and network D
+   t_vars = tf.trainable_variables()
+   d_vars = [var for var in t_vars if 'd_' in var.name]
+   g_vars = [var for var in t_vars if 'g_' in var.name]
+
+   # optimize G
+   G_train_op = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(errG, var_list=g_vars, global_step=global_step)
+
+   # optimize D
+   D_train_op = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(errD, var_list=d_vars, global_step=global_step)
+
+   saver = tf.train.Saver(max_to_keep=1)
+   init  = tf.global_variables_initializer()
+   
+   sess  = tf.Session()
+   sess.run(init)
+
+   # write out logs for tensorboard to the checkpointSdir
+   summary_writer = tf.summary.FileWriter(checkpoint_dir+dataset+'/'+'logs/', graph=tf.get_default_graph())
+
+   # only keep one model
+   #ckpt = tf.train.get_checkpoint_state(checkpoint_dir+dataset+'/')
+
+   tf.add_to_collection('G_train_op', G_train_op)
+   tf.add_to_collection('D_train_op', D_train_op)
+   # restore previous model if there is one
+   #if ckpt and ckpt.model_checkpoint_path:
+   #   print "Restoring previous model..."
+   #   try:
+   #      saver.restore(sess, ckpt.model_checkpoint_path)
+   #      print "Model restored"
+   #   except:
+   #      print "Could not restore model"
+   #      pass
+   
+   ########################################### training portion
+
+   step = sess.run(global_step)
+   
+   while True:
+
+      batch_real_images = random.sample(image_data, batch_size)
+      batch_z = np.random.uniform(-1.0, 1.0, size=[batch_size, 100]).astype(np.float32)
+
+      _, __, Derr, Gerr, summary = sess.run([D_train_op, G_train_op, errD, errG, merged_summary_op],
+         feed_dict={real_images:batch_real_images, z:batch_z})
+
+      # now get all losses and summary *without* performing a training step - for tensorboard
+      #D_loss, G_loss, summary = sess.run([errD, errG, merged_summary_op],
+      #                                    feed_dict={real_images:batch_real_images, z:batch_z})
+
+      #summary_writer.add_summary(summary, step)
+
+      print 'step:',step,'D loss:',Derr,'G_loss:',Gerr
+      
+      step += 1
+
+      if step%5 == 0:
+         print 'Saving model...'
+         saver.save(sess, checkpoint_dir+'checkpoint-'+str(step))
+         saver.export_meta_graph(checkpoint_dir+'checkpoint-'+str(step)+'.meta')
+
+         batch_z  = np.random.uniform(-1.0, 1.0, size=[batch_size, 100]).astype(np.float32)
+         gen_imgs = sess.run([gen_images], feed_dict={z:batch_z})
+
+         num = 0
+         for img in gen_imgs[0]:
+            img = np.asarray(img)
+            img = (img+1.)/2. # these two lines properly scale from [-1, 1] to [0, 255]
+            img *= 255.0/img.max()
+            cv2.imwrite('images/'+dataset+'/'+str(step)+'_'+str(num)+'.png', img)
+            num += 1
+            if num == 20:
+               break
+         print 'Done saving'
+
+
+
+
+
+
+
