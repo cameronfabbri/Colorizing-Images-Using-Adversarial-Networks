2cec862633a8c648b8a2eea29491226df308fe96
Cameron Fabbri <cameronfabbri@gmail.com>
2017-03-02 18:35:23 -0600

    cleaning

--- pix2pix/config/celeba_config.py
+++
@@ -1,6 +0,0 @@
-checkpoint_dir = 'checkpoints/'
-learning_rate  = 5e-5
-batch_size     = 32
-use_labels     = False
-data_dir       = '/home/fabbric/data/images/celeba/images/'
-dataset        = 'celeba'
--- pix2pix/config/imagenet_config.py
+++
@@ -1,5 +0,0 @@
-checkpoint_dir = 'checkpoints/'
-learning_rate  = 5e-5
-batch_size     = 4
-dataset        = 'imagenet'
-use_labels     = False
