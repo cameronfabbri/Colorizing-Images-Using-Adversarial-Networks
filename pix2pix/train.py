f3e145965e992cb8a7d4479530da86eecd0e8015
Cameron Fabbri <cameronfabbri@gmail.com>
2017-03-05 01:35:22 -0600

    bug fixes I hope

--- pix2pix/train.py
+++ pix2pix/train.py
@@ -31,35 +31,45 @@ def buildAndTrain(checkpoint_dir):
    # placeholders for data going into the network
    global_step = tf.Variable(0, name='global_step', trainable=False)
    
+   # training
+   ##############################################
+   train_paths, test_paths, trainData = data_ops.loadTrainData(data_dir, dataset)
+   num_train = trainData.count
+   # The gray 'lightness' channel in range [-1, 1]
+   L_image   = trainData.inputs
+   # The color channels in [-1, 1] range
+   ab_image  = trainData.targets
+   encoded, conv7, conv6, conv5, conv4, conv3, conv2, conv1 = netG_encoder(L_image)
+   # send encoded part to decoder - as well as other layers for skip connections
+   decoded = netG_decoder(encoded, conv7, conv6, conv5, conv4, conv3, conv2, conv1)
+   ##############################################
+   
+   
+   # testing
+   ##############################################
    test_image  = tf.placeholder(tf.float32, shape=(256, 256, 3), name='test_image')
-   test_image  = data_ops.rgb_to_lab(test_image)
 
+   # test image in LAB color space
+   test_image  = data_ops.rgb_to_lab(test_image)
+   
+   # this is the test image in LAB with range [-1, 1]
    test_Lc, test_ac, test_bc = data_ops.preprocess_lab(test_image)
    test_L  = tf.expand_dims(test_Lc, axis=2)
    test_ab = tf.stack([test_ac, test_bc], axis=2)
-
    test_L  = tf.expand_dims(test_L, axis=0)
    test_ab = tf.expand_dims(test_ab, axis=0)
 
-   train_paths, test_paths, trainData = data_ops.loadTrainData(data_dir, dataset)
-   
-   num_train = trainData.count
-   
-   # The gray 'lightness' channel in range [-1, 1]
-   L_image   = trainData.inputs
-   
-   # The color channels in [-1, 1] range
-   ab_image  = trainData.targets
-
-   encoded, conv7, conv6, conv5, conv4, conv3, conv2, conv1 = netG_encoder(L_image)
-
-   # send encoded part to decoder - as well as other layers for skip connections
-   decoded = netG_decoder(encoded, conv7, conv6, conv5, conv4, conv3, conv2, conv1)
-
+   # encode L and decode to ab -> this should be in [-1, 1] range
    enc_test_images, tconv7, tconv6, tconv5, tconv4, tconv3, tconv2, tconv1 = netG_encoder(test_L)
    dec_test_images = netG_decoder(enc_test_images, tconv7, tconv6, tconv5, tconv4, tconv3, tconv2, tconv1)
+   colored_image   = tf.concat([test_L, dec_test_images], axis=3)
    
-   prediction = data_ops.lab_to_rgb(data_ops.augment(dec_test_images, test_L))
+   # now convert from [-1,1] to LAB
+   colored_image = data_ops.deprocess_lab(colored_image[:,:,:,0], colored_image[:,:,:,1], colored_image[:,:,:,2])
+
+   # now convert from LAB to RGB
+   prediction = data_ops.lab_to_rgb(colored_image)
+   ##############################################
 
    '''
       So I don't get confused:
@@ -77,12 +87,11 @@ def buildAndTrain(checkpoint_dir):
    errD_fake = netD(decoded, reuse=True) # gotta pass reuse=True to reuse weights
 
    # weight of how much the l1 loss takes into account 
-   l1_weight = 10.0
+   l1_weight = 100.0
    # total error for the critic
    errD = tf.reduce_mean(errD_real - errD_fake)
    # error for the generator, including the L1 loss
-   #errG = tf.reduce_mean(errD_fake) + l1_loss*l1_weight
-   errG = tf.reduce_mean(errD_fake)
+   errG = tf.reduce_mean(errD_fake) + l1_loss*l1_weight
 
    # tensorboard summaries
    tf.summary.scalar('d_loss', errD)
@@ -97,8 +106,8 @@ def buildAndTrain(checkpoint_dir):
    g_vars = [var for var in t_vars if 'g_' in var.name]
 
    # clip weights in D
-   clip_values = [-0.01, 0.01]
-   #clip_values = [-0.005, 0.005]
+   #clip_values = [-0.01, 0.01]
+   clip_values = [-0.005, 0.005]
    clip_discriminator_var_op = [var.assign(tf.clip_by_value(var, clip_values[0], clip_values[1])) for
       var in d_vars]
 
@@ -144,8 +153,8 @@ def buildAndTrain(checkpoint_dir):
       s = time.time()
       # get the discriminator properly trained at the start
       if step < 25 or step % 500 == 0:
-         n_critic = 100
-      else: n_critic = 5
+         n_critic = 1
+      else: n_critic = 10
 
       # train the discriminator for 5 or 100 runs
       for critic_itr in range(n_critic):
@@ -160,7 +169,7 @@ def buildAndTrain(checkpoint_dir):
       print 'epoch:',epoch_num,'step:',step,'D loss:',D_loss,'G_loss:',G_loss,' time:',time.time()-s
       step += 1
       
-      if step%1000 == 0:
+      if step%3 == 0:
 
          print 'Saving model...'
          saver.save(sess, checkpoint_dir+'checkpoint-'+str(step))
@@ -168,7 +177,7 @@ def buildAndTrain(checkpoint_dir):
          print 'Model saved\n' 
          print 'Evaluating...'
          random.shuffle(test_paths)
-         test_paths = test_paths[:5]
+         test_paths = test_paths[:10]
 
          i = 0
          for t_image in test_paths:
@@ -180,10 +189,11 @@ def buildAndTrain(checkpoint_dir):
             true_image = np.uint8(sess.run(test_image, feed_dict={test_image:img}))
            
             pred_image = sess.run(prediction, feed_dict={test_image:img})[0]
+            pred_image = np.uint8(255*pred_image/pred_image.max())
             misc.imsave('images/'+dataset+'/'+str(step)+'_'+str(i)+'_true.png', true_image)
             misc.imsave('images/'+dataset+'/'+str(step)+'_'+str(i)+'_pred.png', pred_image)
             i += 1
-
+         print 'Done evaluating....running the critic 100 times.'
 
 if __name__ == '__main__':
 
