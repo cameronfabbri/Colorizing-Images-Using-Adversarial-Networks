7c6478df4ebf97b0322276dcb7100e5428e73c70
Cameron Fabbri <cameronfabbri@gmail.com>
2017-03-06 20:27:00 -0600

    training wasserstein

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-#from architecture import netD, netG_encoder, netG_decoder
+import tensorflow as tf
 import numpy as np
 import random
 import ntpath
@@ -6,47 +6,96 @@ import sys
 import cv2
 import os
 import time
-from PIL import Image
 
 from scipy import misc
 from skimage import color
 
 sys.path.insert(0, 'ops/')
+sys.path.insert(0, 'config/')
 
 import data_ops
 
-'''
-   Builds the graph and sets up params, then starts training
-'''
-def buildAndTrain(checkpoint_dir):
+if __name__ == '__main__':
 
+   if len(sys.argv) < 2:
+      print 'You must provide a config file'
+      exit()
+
+   try:
+      config_file = ntpath.basename(sys.argv[1]).split('.py')[0]
+      config = __import__(config_file)
+   except:
+      print 'config',sys.argv[1],'not found'
+      print
+      exit()
+
+   architecture   = config.architecture
+   dataset        = config.dataset
+   checkpoint_dir = 'checkpoints/'+architecture+'_'+dataset+'/'
+   learning_rate  = config.learning_rate
    batch_size     = config.batch_size
    data_dir       = config.data_dir
-   dataset        = config.dataset
+   images_dir     = checkpoint_dir+'images/'
 
-   # placeholders for data going into the network
-   global_step = tf.Variable(0, name='global_step', trainable=False)
+   try: os.mkdir('checkpoints/')
+   except: pass
+   try: os.mkdir(checkpoint_dir)
+   except: pass
+   try: os.mkdir(images_dir)
+   except: pass
    
-   # training
-   ##############################################
-   train_paths, test_paths, trainData = data_ops.loadTrainData(data_dir, dataset, batch_size)
+   global_step = tf.Variable(0, name='global_step', trainable=False)
+  
+   test_paths, trainData = data_ops.loadData(data_dir, dataset, batch_size)
    num_train = trainData.count
+   
    # The gray 'lightness' channel in range [-1, 1]
    L_image   = trainData.inputs
+   
    # The color channels in [-1, 1] range
    ab_image  = trainData.targets
-   encoded, conv7, conv6, conv5, conv4, conv3, conv2, conv1 = netG_encoder(L_image)
-   # send encoded part to decoder - as well as other layers for skip connections
-   decoded = netG_decoder(encoded, conv7, conv6, conv5, conv4, conv3, conv2, conv1)
-   ##############################################
    
+   if architecture == 'pix2pix':
+      import pix2pix
+      encoded, conv7, conv6, conv5, conv4, conv3, conv2, conv1 = netG_encoder(L_image)
+      decoded = netG_decoder(encoded, conv7, conv6, conv5, conv4, conv3, conv2, conv1)
+      # encode L and decode to ab -> this should be in [-1, 1] range
+      enc_test_images, tconv7, tconv6, tconv5, tconv4, tconv3, tconv2, tconv1 = netG_encoder(test_L)
+      dec_test_images = netG_decoder(enc_test_images, tconv7, tconv6, tconv5, tconv4, tconv3, tconv2, tconv1)
+      colored_image   = tf.concat([test_L, dec_test_images], axis=3)
+      
+      # find L1 loss of decoded and original -> this loss is combined with D loss
+      l1_loss = tf.reduce_mean(tf.abs(decoded-ab_image))
    
-   # testing
-   ##############################################
-   test_image  = tf.placeholder(tf.float32, shape=(256, 256, 3), name='test_image')
+      # weight of how much the l1 loss takes into account 
+      l1_weight = 100.0
+   
+      # total error for the critic
+      errD = tf.reduce_mean(errD_real - errD_fake)
+      # error for the generator, including the L1 loss
+      errG = tf.reduce_mean(errD_fake) + l1_loss*l1_weight
+      tf.summary.scalar('encoding_loss', l1_loss)
+  
+   if architecture == 'wasserstein':
+      import wasserstein
+      # generate a colored image
+      gen_img = wasserstein.netG(L_image, batch_size)
 
+      # send real image to D
+      errD_real = wasserstein.netD(ab_image, batch_size)
+
+      # send generated image to D
+      errD_fake = wasserstein.netD(gen_img, batch_size, reuse=True)
+
+      errD = tf.reduce_mean(errD_real - errD_fake)
+
+      errG = tf.reduce_mean(errD_fake)
+
+   # this is testing stuff
+   test_image = tf.placeholder(tf.float32, shape=(256, 256, 3), name='test_image')
+  
    # test image in LAB color space
-   test_image  = data_ops.rgb_to_lab(test_image)
+   test_image = data_ops.rgb_to_lab(test_image)
    
    # this is the test image in LAB with range [-1, 1]
    test_Lc, test_ac, test_bc = data_ops.preprocess_lab(test_image)
@@ -55,45 +104,16 @@ def buildAndTrain(checkpoint_dir):
    test_L  = tf.expand_dims(test_L, axis=0)
    test_ab = tf.expand_dims(test_ab, axis=0)
 
-   # encode L and decode to ab -> this should be in [-1, 1] range
-   enc_test_images, tconv7, tconv6, tconv5, tconv4, tconv3, tconv2, tconv1 = netG_encoder(test_L)
-   dec_test_images = netG_decoder(enc_test_images, tconv7, tconv6, tconv5, tconv4, tconv3, tconv2, tconv1)
-   colored_image   = tf.concat([test_L, dec_test_images], axis=3)
-   
-   # now convert from [-1,1] to LAB
-   colored_image = data_ops.deprocess_lab(colored_image[:,:,:,0], colored_image[:,:,:,1], colored_image[:,:,:,2])
+   if architecture == 'wasserstein':
+      test_colored = wasserstein.netG(test_L, batch_size)
 
-   # now convert from LAB to RGB
-   prediction = data_ops.lab_to_rgb(colored_image)
+   prediction = data_ops.augment(test_colored, test_L)
    prediction = tf.image.convert_image_dtype(prediction, dtype=tf.uint8, saturate=True)
    ##############################################
-
-   '''
-      So I don't get confused:
-      The GRAY image is sent to the encoder, gets encoded, THEN gets decoded but
-      with an EXTRA channel. This is then compared to the ACTUAL ab image via L1.
-   '''
-
-   # find L1 loss of decoded and original -> this loss is combined with D loss
-   l1_loss = tf.reduce_mean(tf.abs(decoded-ab_image))
-
-   # send the real ab image to the critic
-   errD_real = netD(ab_image)
-
-   # send the fake ab image to the critic
-   errD_fake = netD(decoded, reuse=True) # gotta pass reuse=True to reuse weights
-
-   # weight of how much the l1 loss takes into account 
-   l1_weight = 100.0
-   # total error for the critic
-   errD = tf.reduce_mean(errD_real - errD_fake)
-   # error for the generator, including the L1 loss
-   errG = tf.reduce_mean(errD_fake) + l1_loss*l1_weight
 
    # tensorboard summaries
    tf.summary.scalar('d_loss', errD)
    tf.summary.scalar('g_loss', errG)
-   tf.summary.scalar('encoding_loss', l1_loss)
    merged_summary_op = tf.summary.merge_all()
 
    # get all trainable variables, and split by network G and network D
@@ -102,16 +122,16 @@ def buildAndTrain(checkpoint_dir):
    g_vars = [var for var in t_vars if 'g_' in var.name]
 
    # clip weights in D
-   #clip_values = [-0.01, 0.01]
-   clip_values = [-0.005, 0.005]
+   clip_values = [-0.01, 0.01]
+   #clip_values = [-0.005, 0.005]
    clip_discriminator_var_op = [var.assign(tf.clip_by_value(var, clip_values[0], clip_values[1])) for
       var in d_vars]
 
    # optimize G
-   G_train_op = tf.train.RMSPropOptimizer(learning_rate=0.00005).minimize(errG, var_list=g_vars, global_step=global_step, colocate_gradients_with_ops=True)
+   G_train_op = tf.train.RMSPropOptimizer(learning_rate=learning_rate).minimize(errG, var_list=g_vars, global_step=global_step, colocate_gradients_with_ops=True)
 
    # optimize D
-   D_train_op = tf.train.RMSPropOptimizer(learning_rate=0.00005).minimize(errD, var_list=d_vars, global_step=global_step, colocate_gradients_with_ops=True)
+   D_train_op = tf.train.RMSPropOptimizer(learning_rate=learning_rate).minimize(errD, var_list=d_vars, colocate_gradients_with_ops=True)
 
    saver = tf.train.Saver(max_to_keep=1)
    
@@ -166,7 +186,7 @@ def buildAndTrain(checkpoint_dir):
       print 'epoch:',epoch_num,'step:',step,'D loss:',D_loss,'G_loss:',G_loss,' time:',time.time()-s
       step += 1
       
-      if step%1000 == 0:
+      if step%500 == 0:
 
          print 'Saving model...'
          saver.save(sess, checkpoint_dir+'checkpoint-'+str(step))
@@ -180,49 +200,15 @@ def buildAndTrain(checkpoint_dir):
          for t_image in test_paths_:
             img = misc.imread(t_image)
             img = misc.imresize(img, (256,256))
-            colored = sess.run(dec_test_images, feed_dict={test_image:img})
+            colored = sess.run(prediction, feed_dict={test_image:img})
 
             test_L_image = sess.run(test_L, feed_dict={test_image:img})
             true_image   = np.uint8(sess.run(test_image, feed_dict={test_image:img}))
            
             pred_image = sess.run(prediction, feed_dict={test_image:img})[0]
-            misc.imsave('images/'+dataset+'/'+str(step)+'_'+str(i)+'_true.png', true_image)
-            misc.imsave('images/'+dataset+'/'+str(step)+'_'+str(i)+'_pred.png', pred_image)
+            misc.imsave(images_dir+str(step)+'_'+str(i)+'_true.png', true_image)
+            misc.imsave(images_dir+str(step)+'_'+str(i)+'_pred.png', pred_image)
             i += 1
          print 'Done evaluating....running the critic 100 times.'
          exit()
-if __name__ == '__main__':
-
-   if len(sys.argv) < 3:
-      print 'You must provide a config file'
-      exit()
-
-   try:
-      config_file = ntpath.basename(sys.argv[1]).split('.py')[0]
-      config = __import__(config_file)
-   except:
-      print 'config',sys.argv[1],'not found'
-      print
-      exit()
-
-   import tensorflow as tf
-   checkpoint_dir = config.checkpoint_dir
-   learning_rate  = config.learning_rate
-   batch_size     = config.batch_size
-   data_dir       = config.data_dir
-   dataset        = config.dataset
-   if checkpoint_dir[-1] is not '/': checkpoint_dir+='/'
-   try: os.mkdir(checkpoint_dir)
-   except: pass
-   try: os.mkdir(checkpoint_dir+dataset)
-   except: pass
-   try: os.mkdir('images/')
-   except: pass
-   try: os.mkdir('images/'+dataset)
-   except: pass
-   
-   checkpoint_dir = checkpoint_dir+dataset+'/'
-   
-   buildAndTrain(checkpoint_dir)
-
 
--- wasserstein.py
+++ wasserstein.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import tensorflow.contrib.slim as slim
 import sys
 
-import config
+multi_gpu = True
 
 '''
    Leaky RELU
@@ -11,75 +11,83 @@ import config
 def lrelu(x, leak=0.2, name='lrelu'):
    return tf.maximum(leak*x, x)
 
-def netG(z, batch_size):
+'''
+   Model following http://cs231n.stanford.edu/reports2016/224_Report.pdf
+'''
+def netG(L_image, batch_size):
 
+   if multi_gpu: gpu_num = 1
+   else: gpu_num = 0
+   with tf.device('/gpu:'+str(gpu_num)):
+      conv1 = slim.convolution(L_image, 32, 3, stride=1, activation_fn=tf.identity, scope='g_conv1')
+      conv1 = lrelu(conv1)
+
+      conv2 = slim.convolution(conv1, 32, 3, stride=1, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='g_conv2')
+      conv2 = lrelu(conv2)
+
+      conv3 = slim.convolution(conv2, 64, 3, stride=1, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='g_conv3')
+      conv3 = lrelu(conv3)
+
+      conv4 = slim.convolution(conv3, 32, 1, stride=1, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='g_conv4')
+      conv4 = lrelu(conv4)
+
+      conv5 = slim.convolution(conv4, 2, 1, stride=1, activation_fn=tf.identity, scope='g_conv5')
+      conv5 = tf.nn.tanh(conv5)
+   
    print 'GENERATOR'
-   z = slim.fully_connected(z, 4*4*1024, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='g_z')
-   z = tf.reshape(z, [batch_size, 4, 4, 1024])
-   print 'z:',z
-
-   print 'z:',z
-   conv1 = slim.convolution2d_transpose(z, 512, 5, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='g_conv1')
-   conv1 = tf.nn.relu(conv1)
    print 'conv1:',conv1
-
-   conv2 = slim.convolution2d_transpose(conv1, 256, 5, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='g_conv2')
-   conv2 = tf.nn.relu(conv2)
    print 'conv2:',conv2
-
-   conv3 = slim.convolution2d_transpose(conv2, 128, 5, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='g_conv3')
-   conv3 = tf.nn.relu(conv3)
    print 'conv3:',conv3
-
-   conv4 = slim.convolution2d_transpose(conv3, 3, 5, stride=2, activation_fn=tf.identity, scope='g_conv4')
-   conv4 = tf.nn.tanh(conv4)
    print 'conv4:',conv4
-   print
+   print 'conv5:',conv5
    print 'END G'
    print
-
+   
    tf.add_to_collection('vars', conv1)
    tf.add_to_collection('vars', conv2)
    tf.add_to_collection('vars', conv3)
    tf.add_to_collection('vars', conv4)
+   tf.add_to_collection('vars', conv5)
 
-   return conv4 
+   return conv5 
 
 
 '''
    Discriminator network
 '''
 def netD(input_images, batch_size, reuse=False):
-   print 'DISCRIMINATOR' 
    sc = tf.get_variable_scope()
    with tf.variable_scope(sc, reuse=reuse):
+      if multi_gpu: gpu_num = 1
+      else: gpu_num = 0
+      with tf.device('/gpu:'+str(gpu_num)):
+         conv1 = slim.convolution(input_images, 64, 5, stride=2, activation_fn=tf.identity, scope='d_conv1')
+         conv1 = lrelu(conv1)
 
-      print 'input images:',input_images
-      conv1 = slim.convolution(input_images, 64, 5, stride=2, activation_fn=tf.identity, scope='d_conv1')
-      conv1 = lrelu(conv1)
-      print 'conv1:',conv1
-
-      conv2 = slim.convolution(conv1, 128, 5, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='d_conv2')
-      conv2 = lrelu(conv2)
-      print 'conv2:',conv2
-   
-      conv3 = slim.convolution(conv2, 256, 5, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='d_conv3')
-      conv3 = lrelu(conv3)
-      print 'conv3:',conv3
-
-      conv4 = slim.convolution(conv3, 512, 5, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='d_conv4')
-      conv4 = lrelu(conv4)
-      print 'conv4:',conv4
-
-      conv5 = slim.convolution(conv4, 1, 4, stride=2, activation_fn=tf.identity, scope='d_conv5')
-      print 'conv5:',conv5
+         conv2 = slim.convolution(conv1, 128, 5, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='d_conv2')
+         conv2 = lrelu(conv2)
       
+         conv3 = slim.convolution(conv2, 256, 5, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='d_conv3')
+         conv3 = lrelu(conv3)
+
+         conv4 = slim.convolution(conv3, 512, 5, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='d_conv4')
+         conv4 = lrelu(conv4)
+
+         conv5 = slim.convolution(conv4, 1, 4, stride=2, activation_fn=tf.identity, scope='d_conv5')
+      
+      print 'DISCRIMINATOR' 
+      print 'input images:',input_images
+      print 'conv1:',conv1
+      print 'conv2:',conv2
+      print 'conv3:',conv3
+      print 'conv4:',conv4
+      print 'conv5:',conv5
       print 'END D\n'
       
       tf.add_to_collection('vars', conv1)
       tf.add_to_collection('vars', conv2)
       tf.add_to_collection('vars', conv3)
       tf.add_to_collection('vars', conv4)
+      tf.add_to_collection('vars', conv5)
       
-      return conv4
-
+      return conv5
