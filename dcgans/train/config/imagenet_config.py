3d4da6bbace76f408f115350ed8d853efd4790a1
Cameron Fabbri <cameronfabbri@gmail.com>
2017-03-06 18:21:12 -0600

    update

--- dcgans/train/config/imagenet_config.py
+++
@@ -1,7 +0,0 @@
-dataset        = 'imagenet'
-dataset_location = '/home/fabbric/data/images/imagenet/ILSVRC/224x224_images/CLS-LOC/imagenet_bw.pkl'
-batch_size     = 6
-checkpoint_dir = '/home/fabbric/Research/Colorizing-Images-Using-Adversarial-Networks/dcgans/train/models/'
-learning_rate  = 0.0002
-
-
--- dcgans/train/config/mnist_config.py
+++
@@ -1,7 +0,0 @@
-dataset        = 'mnist'
-dataset_loc    = '/home/fabbric/data/images/mnist/mnist.pkl'
-batch_size     = 128
-checkpoint_dir = '/home/fabbric/Research/Colorizing-Images-Using-Adversarial-Networks/dcgans/train/models/'
-learning_rate  = 0.0002
-
-
