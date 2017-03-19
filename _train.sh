341f1f3321bba5aebb5952c1849982b01f3aa4bd
Cameron Fabbri <cameronfabbri@gmail.com>
2017-03-18 21:35:17 -0500

    update

--- _train.sh
+++ _train.sh
@@ -1,2 +1,1 @@
-#python train.py --ARCHITECTURE=pix2pix --GAN_EPOCHS=4 --DATASET=celeba --DATA_DIR=/home/fabbric/data/images/celeba/original/ --BATCH_SIZE=4 --NUM_GPU=1 --JITTER=1 --LOSS_METHOD=gan --SIZE=256 --GAN_LR=1e-4 --L1_WEIGHT=100.0
-python train.py --ARCHITECTURE=pix2pix --GAN_EPOCHS=4 --DATASET=celeba --DATA_DIR=/home/fabbric/data/images/celeba/original/ --BATCH_SIZE=4 --NUM_GPU=1 --JITTER=1 --LOSS_METHOD=gan --SIZE=256 --GAN_LR=1e-4 --L1_WEIGHT=0.0 --L2_WEIGHT=0.0
+python train.py --ARCHITECTURE=pix2pix --GAN_EPOCHS=4 --DATASET=celeba --DATA_DIR=/mnt/data2/images/celeba/images/ --BATCH_SIZE=4 --NUM_GPU=1 --JITTER=1 --LOSS_METHOD=gan --SIZE=256 --GAN_LR=1e-4 --L1_WEIGHT=0.0 --L2_WEIGHT=1.0
