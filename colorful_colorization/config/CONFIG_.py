340f13958c7ea978c39f9a364c4f76f3528edd27
xahidbuffon <xahid.buffon@gmail.com>
2017-03-26 16:37:01 -0500

    first commit

---
+++ colorful_colorization/config/CONFIG_.py
@@ -0,0 +1,12 @@
+# TODO 
+
+
+PRETRAIN_EPOCHS = 0
+DATASET = 'celeba'
+BATCH_SIZE = 32
+PRETRAIN_LR = 2e-5
+L_rate   = 1e-5
+NUM_GPU = 1
+LOAD_MODEL = False
+#DATA_DIR = '/home/xahid/datasets/tensor_flow/celeba/images/'
+DATA_DIR = '/mnt/data2/images/celeba/images/'
