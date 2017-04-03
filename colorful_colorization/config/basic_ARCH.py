53cbea7ab8c3ff96e9f9dee1586ba87e77907f9b
xahidbuffon <xahid.buffon@gmail.com>
2017-04-02 20:50:31 -0500

    trying places 2

--- colorful_colorization/config/basic_ARCH.py
+++ colorful_colorization/config/basic_ARCH.py
@@ -7,7 +7,7 @@ def lrelu(x, leak=0.2, name='lrelu'):
    return tf.maximum(leak*x, x)
 
 
-def netG(L_image, batch_size, num_gpu):
+def netG(L_image, num_gpu):
    if   num_gpu == 0: Machines = ['/cpu:0']
    elif num_gpu == 1: Machines = ['/gpu:0']
    elif num_gpu == 2: Machines = ['/gpu:0', '/gpu:1']
